f264492e3236a2a1ef4ff54d13989e9902c89b7e
Carl Busch <carl@123busch.de>
2023-11-17 17:00:05 +0100

    keeped adding styling and everything to my webpage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list) #change later to list_path(@list)
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
@@ -29,6 +29,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
