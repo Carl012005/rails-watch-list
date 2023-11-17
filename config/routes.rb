cf9979da7b9766a2a14f79ba5c4a2011351cfd96
Carl Busch <carl@123busch.de>
2023-11-17 12:09:24 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get '/lists/new', to: 'lists#new', as: :new_list
   post '/lists', to: 'lists#create'
   get '/lists/:id', to: 'lists#show', as: :list
+  delete '/lists/:id', to: 'lists#destroy', as: :destroy_list
   get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
   post '/lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
+  delete '/bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
 end
